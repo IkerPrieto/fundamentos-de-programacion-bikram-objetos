7c24d3534a00bcb96f02b811497ef57fcfe29e8d
IkerPrieto <iprietod01@gmail.com>
2025-03-26 19:38:15 +0100

    Terminado

---
+++ index.js
@@ -0,0 +1,124 @@
+// 1
+const Coche = {
+    marca: 'marca',
+    modelo: 'modelo',
+    matricula: 'matrícula'
+}
+
+// 2
+const Casa = {
+    codPostal: 'codPostal',
+    calle: 'calle',
+    portal: 'portal',
+    piso: 'piso'
+}
+
+// 3
+const FullStackDeveloper = {
+    lenguajes: ['arrayLenguajes'],
+    proyectos: ['arrayProyectos']
+}
+
+// 4
+const Perro = {
+    nombre: '',
+    raza: '',
+    color: '',
+    edad: '',
+    ladrar: function() {
+        return console.log('guau')
+    },
+    popo: function() {
+        return Math.random()*3
+    }
+}
+
+// 5
+let marcaPortatil = Portatil.marca
+
+// 6
+let marcaPortatil2 = Portatil.marca
+
+// 7
+let grupos = Concierto.grupos
+
+// 8
+let RGB = [Led.rojo, Led.verde, Led.azul]
+
+// 9 
+Portatil.modelo = 'P345'
+
+// 10
+Concierto.cartelera.push("Guns N' Roses")
+
+// 11
+Concierto.fecha = new Date()
+
+// 12
+Impresora.imprimiendo = ['nombreArchivo', 'copias', 'numPaginas']
+
+// 13
+const Noticia = {
+    titular: '',
+    cuerpo: ''
+}
+
+// 14
+const Persona = {
+    nombre: '',
+    apellidos: '',
+    edad: ''
+}
+
+// 15
+const Avion = {
+    numPasajeros: '',
+    despegar: function() {
+        return console.log('despegando')
+    },
+    volar: function() {
+        return console.log('llegando al destino')
+    },
+    aterrizar: function() {
+        return console.log('aterrizando')
+    }
+}
+
+// 16
+const Paquete = {
+    contenido: []
+}
+
+// 17
+const Pais = {
+    numHabitantes: '',
+    continente: '',
+    gentilicio: ''
+}
+
+// 18
+let codError = O_Error.codigo
+
+// 19
+let integrantes = Grupo.integrantes
+
+// 20
+let nivelesTinta = Impresora.tinta
+
+// 21
+let pixeles = Pantalla.pixeles
+
+// 22
+let especificaciones = Movil.especificaciones
+
+// 23
+Grupo.numIntegrantes = 5
+
+// 24
+Pantalla.dimensiones = '1920x1080'
+
+//25
+Led.encendido = true
+
+// 26
+Movil.temperatura = '20º'
